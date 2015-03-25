db0be389d32cd7a13bcdcc028dbeb801c9d862c5
ventuno <ernestom@feynman.local>
2015-03-24 18:57:03 -0700

    Remove non-ascii chars. Minor refactoring.

--- kwadrat.py
+++ kwadrat.py
@@ -1,11 +1,11 @@
 # Copyright (C) 2015 kwadrat
 # License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
 
-#A simple project inspired by Zbigniew Rybczyński's short film Kwadrat (Square).
+#A simple project inspired by Zbigniew Rybczynski's short film Kwadrat (Square).
 #Watch the film: http://artmuseum.pl/pl/filmoteka/praca/rybczynski-zbigniew-kwadrat
 #Read a description: http://www.zbigvision.com/Kwadrat.html
 #ZBIG VISION website: http://www.zbigvision.com/
-#kwadrat lets you turn a set of images in a set of frames following the process used by Zbig Rybczyński when making the short film Kwadrat in 1972.
+#kwadrat lets you turn a set of images in a set of frames following the process used by Zbig Rybczynski when making the short film Kwadrat in 1972.
 #0) For every picture in the loop
 #1) Take a square picture of size l
     #2) Build a grid of size l on top of the image
@@ -25,14 +25,14 @@ KWADRAT_WHITE = 255
 KWADRAT_PROGRAM_NAME = "kwadrat"
 KWADRAT_BASE_OUTPUT_FILE_NAME = "kwadrat"
 
-def get_max_value_in_range(range_w, range_h, img):
+def photograph_instersects_square(range_w, range_h, img):
     max = 0
     for x in xrange(range_w[0], range_w[1]):
         for y in xrange(range_h[0], range_h[1]):
             current = img.getpixel((x, y))
-            if current > max:
-                max = current
-    return max
+            if current == 255:
+                return True
+    return False
 
 def set_value_in_range(range_w, range_h, value, img):
     for x in xrange(range_w[0], range_w[1]):
@@ -66,12 +66,12 @@ def make_kwadrat(photographs):
                 for y in xrange(0,(ratio)):
                     h_left = length/ratio*y
                     h_right = length/ratio*(y+1)
-                    max = get_max_value_in_range((w_left, w_right), (h_left, h_right), photograph_copy)
-                    if max == 0:
-                        set_value_in_range((w_left, w_right), (h_left, h_right), KWADRAT_BLACK, photograph_copy)
-                    else:
+                    intersects = photograph_instersects_square((w_left, w_right), (h_left, h_right), photograph_copy)
+                    if intersects:
                         set_value_in_range((w_left, w_right), (h_left, h_right), KWADRAT_WHITE, photograph_copy)
-                    print "{} -- iteration: ({}, {}) - ({}, {}) - {}".format(KWADRAT_PROGRAM_NAME, w_left, w_right, h_left, h_right, max)
+                    else:
+                        set_value_in_range((w_left, w_right), (h_left, h_right), KWADRAT_BLACK, photograph_copy)
+                    print "{} -- iteration: ({}, {}) - ({}, {}) - {}".format(KWADRAT_PROGRAM_NAME, w_left, w_right, h_left, h_right, intersects)
             save_kwadrat_photograph(sequence, current_length, photograph_copy)
             sequence += 1
         current_length = current_length/2
